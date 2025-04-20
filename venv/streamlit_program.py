e4372d267c87f29228617777f4b402d9fb99c238
hari291-aishu <143933929+hari291-aishu@users.noreply.github.com>
2025-04-20 18:30:30 +0000

    created a new streamlit file

---
+++ venv/streamlit_program.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+st.title("You are Marvel Messia LOL!!!")
+
+st.header("Tom needs access to the data")
+
+st.subheader("tom is talking")
+
+
+
